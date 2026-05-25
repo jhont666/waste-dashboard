6a6eb1b9f0dbf02d0370bf86fd1d578faeb1eb0c
jhont666 <129878776+jhont666@users.noreply.github.com>
2026-05-25 09:14:18 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function App() {
     { id: 'loc6', name: 'RT 06' }, { id: 'loc7', name: 'RT 07' }, { id: 'loc8', name: 'RT 08' }, { id: 'loc9', name: 'RT 09' }
   ];
   const wasteTypes = [
-    { id: 'wt1', name: 'Organik' }, { id: 'wt2', name: 'Anorganik' }, { id: 'wt3', name: 'Kertas' }, { id: 'wt4', name: 'Sampah Residu' }, { id: 'wt5', name: 'Kardus' }
+    { id: 'wt1', name: 'Organik', 1 }, { id: 'wt2', name: 'Anorganik', 0 }, { id: 'wt3', name: 'Kertas', 0 }, { id: 'wt4', name: 'Sampah Residu', 0 }, { id: 'wt5', name: 'Kardus', 1 }
   ];
 
   useEffect(() => {
